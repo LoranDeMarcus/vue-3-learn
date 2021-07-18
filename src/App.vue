1dca08088592f1e3e78b2c50d79a8236f4dfb3d7
Artyom Kuzikov <akuzikov@sberhealth.ru>
2021-07-18 21:07:46 +0300

    created vue app

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+  posts: [
+    {
+      id: 1,
+      title: 'JavaScript',
+      body: 'Описание поста'
+    },
+    {
+      id: 2,
+      title: 'JavaScript 2',
+      body: 'Описание поста 2'
+    },
+    {
+      id: 3,
+      title: 'JavaScript 3',
+      body: 'Описание поста 3'
+    }
+  ]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 10px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  margin-bottom: 15px;
+  padding: 15px;
+  border: 2px solid teal;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
+import App from './App'
 
 createApp(App).mount('#app')
 
-// https://www.youtube.com/watch?v=XzLuMtDelGk&t=44s      11.55
+// https://www.youtube.com/watch?v=XzLuMtDelGk&t=44s 23:02
